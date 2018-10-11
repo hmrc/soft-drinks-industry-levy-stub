9d84fee4d6d34644679b28988f7237f8e7347bb4
adamdye-hmrc <adam.dye@digital.hmrc.gov.uk>
2018-10-11 10:33:16 +0100

    Changed import.

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -15,10 +15,10 @@ object MicroServiceBuild extends Build with MicroService {
     ws,
     "uk.gov.hmrc" %% "stub-data-generator" % "0.5.3",
     "uk.gov.hmrc" %% "microservice-bootstrap" % "6.18.0",
+    "uk.gov.hmrc" %% "play-ui" % "7.22.0",
     "uk.gov.hmrc" %% "domain" % "5.2.0",
     "com.github.fge" % "json-schema-validator" % "2.2.6",
-    "org.scalacheck" %% "scalacheck" % "1.14.0",
-    "uk.gov.hmrc" %% "play-ui" % "7.22.0"
+    "org.scalacheck" %% "scalacheck" % "1.14.0"
   )
 
   def test(scope: String = "test,it") = Seq(
