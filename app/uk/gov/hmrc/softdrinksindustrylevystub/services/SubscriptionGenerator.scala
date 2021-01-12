921cb11f41cb80850054e3f472520dbf0fb0c67e
calinursaciuc <31273736+calinursaciuc@users.noreply.github.com>
2021-01-12 16:43:17 +0000

    [CU][DLS-3846]: Fix stub + year changes

--- app/uk/gov/hmrc/softdrinksindustrylevystub/services/SubscriptionGenerator.scala
+++ app/uk/gov/hmrc/softdrinksindustrylevystub/services/SubscriptionGenerator.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -106,7 +106,7 @@ object SubscriptionGenerator {
     address     <- addressGen
     ref         <- Gen.posNum[Int]
     tradingName <- orgNameGen
-    closureDate <- Gen.date(2016, 2020)
+    closureDate <- Gen.date(2016, 2024)
   } yield Site(address, Some(ref.toString), Some(tradingName.toString), Some(closureDate))
 
   private lazy val addressGen: Gen[Address] = {
