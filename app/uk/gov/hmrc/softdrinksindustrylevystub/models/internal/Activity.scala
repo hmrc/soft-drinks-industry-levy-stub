f7ef435ad15d9f13edf32371abe634ebf4cb1809
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-02-13 14:03:01 +0000

    DLS-9975: Removes unused activity properties

--- app/uk/gov/hmrc/softdrinksindustrylevystub/models/internal/Activity.scala
+++ app/uk/gov/hmrc/softdrinksindustrylevystub/models/internal/Activity.scala
@@ -29,8 +29,6 @@ sealed trait Activity {
   def isImporter: Boolean
   def isVoluntaryRegistration: Boolean = isProducer && !isLarge && !isImporter && !isContractPacker
   def isSmallProducer: Boolean = isProducer && !isLarge
-  def isLargeNoImports: Boolean = isProducer && isLarge && !isImporter
-  def isLargeImportCopacker: Boolean = isProducer && isLarge && isImporter && isContractPacker
 }
 
 case class InternalActivity(activity: Map[ActivityType.Value, LitreBands], isLarge: Boolean) extends Activity {
