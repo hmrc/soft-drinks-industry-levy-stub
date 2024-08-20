ebab5c392b04727f6f10e65781250ad7b687b0fc
jchrisp95 <jake.d.chrisp@accenture.com>
2024-08-20 08:46:47 +0100

    DLS-10272: Updates to sdil-stub appDependencies & plugins.sbt

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,23 +2,23 @@ import sbt._
 
 object AppDependencies {
 
-  private val playSuffix = "-play-30"
-  private val bootstrapVersion = "8.4.0"
+  private val playVersion = "-play-30"
+  private val bootstrapVersion = "8.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "com.github.fge"         %  "json-schema-validator"         % "2.2.6",
-    "uk.gov.hmrc"            %% s"domain$playSuffix"            % "9.0.0",
-    "uk.gov.hmrc"            %% s"bootstrap-backend$playSuffix" % bootstrapVersion,
+    "uk.gov.hmrc"            %% s"domain$playVersion"            % "10.0.0",
+    "uk.gov.hmrc"            %% s"bootstrap-backend$playVersion" % bootstrapVersion,
     "org.scala-lang.modules" %% "scala-parallel-collections"    % "1.0.4",
     "uk.gov.hmrc"            %% "stub-data-generator"           % "1.1.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.mockito"            %% "mockito-scala-scalatest"    % "1.17.30",
-    "org.scalatestplus"      %% "scalacheck-1-17"            % "3.2.17.0",
-    "org.scalatestplus.play" %% "scalatestplus-play"         % "7.0.0",
-    "uk.gov.hmrc"            %% s"bootstrap-test$playSuffix" % bootstrapVersion
+    "org.mockito"            %% "mockito-scala-scalatest"    % "1.17.37",
+    "org.scalatestplus"      %% "scalacheck-1-17"            % "3.2.18.0",
+    "org.scalatestplus.play" %% "scalatestplus-play"         % "7.0.1",
+    "uk.gov.hmrc"            %% s"bootstrap-test$playVersion" % bootstrapVersion
   ).map(_ % "test")
 
   def apply(): Seq[ModuleID] = compile ++ test
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,9 +3,9 @@ resolvers += "HMRC-open-artefacts-maven" at "https://open.artefacts.tax.service.
 resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
 resolvers += Resolver.jcenterRepo
 
-addSbtPlugin("uk.gov.hmrc"        %  "sbt-auto-build"         % "3.18.0")
-addSbtPlugin("uk.gov.hmrc"        %  "sbt-distributables"     % "2.4.0")
-addSbtPlugin("org.playframework"  %  "sbt-plugin"             % "3.0.1")
+addSbtPlugin("uk.gov.hmrc"        %  "sbt-auto-build"         % "3.22.0")
+addSbtPlugin("uk.gov.hmrc"        %  "sbt-distributables"     % "2.5.0")
+addSbtPlugin("org.playframework"  %  "sbt-plugin"             % "3.0.5")
 addSbtPlugin("org.scalastyle"     %% "scalastyle-sbt-plugin"  % "1.0.0" exclude("org.scala-lang.modules", "scala-xml_2.12"))
 addSbtPlugin("org.scalameta" % "sbt-scalafmt"   % "2.5.2")
 ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
