90442f25b2487ba3e926bfc8cf556908796b769e
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2024-08-29 14:15:29 +0100

    DLS-10847 addressed platOp warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,15 +11,14 @@ object AppDependencies {
     "uk.gov.hmrc"            %% s"domain$playVersion"            % "10.0.0",
     "uk.gov.hmrc"            %% s"bootstrap-backend$playVersion" % bootstrapVersion,
     "org.scala-lang.modules" %% "scala-parallel-collections"    % "1.0.4",
-    "uk.gov.hmrc"            %% "stub-data-generator"           % "1.1.0"
+    "uk.gov.hmrc" %% "stub-data-generator" % "1.1.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "org.mockito"            %% "mockito-scala-scalatest"    % "1.17.37",
     "org.scalatestplus"      %% "scalacheck-1-17"            % "3.2.18.0",
     "org.scalatestplus.play" %% "scalatestplus-play"         % "7.0.1",
-    "uk.gov.hmrc"            %% s"bootstrap-test$playVersion" % bootstrapVersion,
-    "uk.gov.hmrc" %% "stub-data-generator" % "1.1.0"
+    "uk.gov.hmrc"            %% s"bootstrap-test$playVersion" % bootstrapVersion
   ).map(_ % "test")
 
   def apply(): Seq[ModuleID] = compile ++ test
