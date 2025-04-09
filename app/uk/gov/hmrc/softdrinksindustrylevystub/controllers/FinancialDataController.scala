97324b924216af3c89512bf52fca6ed401140f67
ZaraFarzan <177644563+ZaraFarzan@users.noreply.github.com>
2025-04-09 22:09:41 +0100

    DLS-11481-test: uggrade to scala 3

--- app/uk/gov/hmrc/softdrinksindustrylevystub/controllers/FinancialDataController.scala
+++ app/uk/gov/hmrc/softdrinksindustrylevystub/controllers/FinancialDataController.scala
@@ -31,7 +31,7 @@ class FinancialDataController @Inject() (cc: ControllerComponents) extends Backe
   val logger = Logger("FinancialDataController")
   val canned = CannedFinancialData.canned
 
-  implicit val sdilEnum: Enumerable[String] = SdilNumberTransformer.sdilRefEnum
+  given Enumerable[String] = SdilNumberTransformer.sdilRefEnum
 
   def test(
     sdilRef: String,
@@ -43,8 +43,8 @@ class FinancialDataController @Inject() (cc: ControllerComponents) extends Backe
     logger.info(
       s"Query parameters onlyOpenItems=$onlyOpenItems, includeLocks=$includeLocks, calculateAccruedInterest=$calculateAccruedInterest, customerPaymentInformation= $customerPaymentInformation"
     )
-//    val id = sdilRef.toLong % canned.size
-    val id = math.abs(sdilRef.hashCode % canned.size)
+    val numericSdilRef = sdilRef.replaceAll("[^0-9]", "")
+    val id = numericSdilRef.toLong % canned.size
 
     canned(id.toInt) match {
       case (file, Left(e)) => throw new IllegalStateException(s"unable to parse $file", e)
