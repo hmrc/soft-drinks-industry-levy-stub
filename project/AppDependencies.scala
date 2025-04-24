fac63daa1be2333b295f8a78d964b714ffa98a81
ZaraFarzan <177644563+ZaraFarzan@users.noreply.github.com>
2025-04-24 15:30:59 +0100

    DLS-11481: upgrade to scala 3

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,16 +7,16 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "com.github.fge"         %  "json-schema-validator"         % "2.2.6",
+    "com.github.fge"         %  "json-schema-validator"          % "2.2.6",
     "uk.gov.hmrc"            %% s"domain$playVersion"            % "10.0.0",
     "uk.gov.hmrc"            %% s"bootstrap-backend$playVersion" % bootstrapVersion,
-    "org.scala-lang.modules" %% "scala-parallel-collections"    % "1.2.0",
-    "uk.gov.hmrc" %% "stub-data-generator" % "1.5.0"
+    "org.scala-lang.modules" %% "scala-parallel-collections"     % "1.2.0",
+    "uk.gov.hmrc"            %% "stub-data-generator"            % "1.5.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatestplus"      %% "scalacheck-1-17"            % "3.2.18.0",
-    "org.scalatestplus.play" %% "scalatestplus-play"         % "7.0.1",
+    "org.scalatestplus"      %% "scalacheck-1-17"             % "3.2.18.0",
+    "org.scalatestplus.play" %% "scalatestplus-play"          % "7.0.1",
     "uk.gov.hmrc"            %% s"bootstrap-test$playVersion" % bootstrapVersion
   ).map(_ % "test")
 
