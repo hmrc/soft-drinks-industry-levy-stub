8ac939d4f4418c35d5270b8812d663cb2533c3bf
ZaraFarzan <177644563+ZaraFarzan@users.noreply.github.com>
2025-05-06 09:27:39 +0100

    11437:[SDIL] Apply Scala 3 Update to the soft-drinks-industry-levy-stub

--- app/uk/gov/hmrc/softdrinksindustrylevystub/models/internal/Activity.scala
+++ app/uk/gov/hmrc/softdrinksindustrylevystub/models/internal/Activity.scala
@@ -29,6 +29,8 @@ sealed trait Activity {
   def isSmallProducer: Boolean = isProducer && !isLarge
   def isLargeNoImports: Boolean = isProducer && isLarge && !isImporter
   def isLargeImportCopacker: Boolean = isProducer && isLarge && isImporter && isContractPacker
+  def isSmallProducerContractPacker: Boolean = isProducer && isContractPacker && !isLarge
+  def isSmallContractPacker: Boolean = !isProducer && isContractPacker && !isLarge
 }
 
 case class InternalActivity(activity: Map[ActivityType.Value, LitreBands], isLarge: Boolean) extends Activity {
