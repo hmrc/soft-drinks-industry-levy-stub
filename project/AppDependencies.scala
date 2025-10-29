2495cee7512b78ed6a3bddd073d135a7968ce5dc
ZaraFarzan <177644563+ZaraFarzan@users.noreply.github.com>
2025-10-29 10:20:22 +0000

    DLS-12109:[SDIL] Remove testOnlyDoNotUseInAppConf.Routes if repo does not use any test routes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,14 +11,13 @@ object AppDependencies {
     "uk.gov.hmrc"            %% s"domain-$playVersion"            % "13.0.0",
     "com.github.fge"         %  "json-schema-validator"           % "2.2.6",
     "org.scala-lang.modules" %% "scala-parallel-collections"      % "1.2.0",
-    "uk.gov.hmrc"            %% "stub-data-generator"             % "1.5.0",
+    "uk.gov.hmrc"            %% "stub-data-generator"             % "1.5.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion" % bootstrapVersion,
     "org.scalatestplus"      %% "scalacheck-1-17"              % "3.2.18.0",
     "org.scalatestplus.play" %% "scalatestplus-play"           % "7.0.2",
-    "uk.gov.hmrc"            %% "stub-data-generator"          % "1.5.0",
   ).map(_ % "test")
 
   def apply(): Seq[ModuleID] = compile ++ test
